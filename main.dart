8ebdac38782dd8effaf79c5256005d91f8c0f200
IceMelon1 <avitalnota@gmail.com>
2026-09-07 18:09:33 +0300

    first commit

--- main.dart
+++ main.dart
@@ -1,25 +1,38 @@
 import 'dart:convert';
 import 'dart:io';
-
+import 'dart:math';
 
 void main() {
-  double totalTemp = 0;
+  //find the average number in a list
+  double avrg(List numbers) {
+    double totalTemp = 0;
+    double avrgTemp = 0;
 
+    for (num number in numbers) {
+      totalTemp += number;
+    }
+
+    avrgTemp = totalTemp / numbers.length;
+    return avrgTemp;
+  }
+
+  //decoding the JSON file
   File file = File('readings.json');
   String stringJson = file.readAsStringSync();
   List<dynamic> listJson = jsonDecode(stringJson);
-  List<Map<String, dynamic>> mapListJson = List<Map<String, dynamic>>.from(listJson);
-  //print(mapListJson);
 
-  for(Map indMap in mapListJson) {
-    for(var key in indMap.keys) {
-      if(key == "teperture"){
-        double temp = indMap['temperature'];
-        totalTemp += temp;
-      }
+  List<double> allTemps = [];
+
+  //adding all the temperature to a list
+  for (Map map in listJson) {
+    if (map.containsKey("temperature")) {
+      num temp = map["temperature"];
+      allTemps.add(temp);
     }
   }
 
-  double avrgTemp = totalTemp / mapListJson.length;
-  print(avrgTemp);
+  //prints the answers
+  print("Average temperature: ${avrg(allTemps)}");
+  print("Highest temperature: ${allTemps.reduce(max)}");
+  print("lowest temperature: ${allTemps.reduce(min)}");
 }
