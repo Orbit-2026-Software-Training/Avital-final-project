52bade55996761998960d6cc473a80993b4ac7e3
IceMelon1 <avitalnota@gmail.com>
2026-09-07 20:04:06 +0300

    second task complete

--- main.dart
+++ main.dart
@@ -3,17 +3,27 @@ import 'dart:io';
 import 'dart:math';
 
 void main() {
+
   //find the average number in a list
   num avrg(List numbers) {
     num totalTemp = 0;
     num avrgTemp = 0;
-
     for (num number in numbers) {
       totalTemp += number;
     }
-
     avrgTemp = totalTemp / numbers.length;
     return avrgTemp;
+  }
+
+  //checks how much times the engine temperature was over 25
+  int timesTheEnginRanWell(List numbers) {
+    int numOfGoodTimes = 0;
+    for(double number in numbers) {
+      if(number > 25) {
+        numOfGoodTimes++;
+      }
+    }
+    return numOfGoodTimes;
   }
 
   //decoding the JSON file
@@ -35,4 +45,5 @@ void main() {
   print("Average temperature: ${avrg(allTemps)}");
   print("Highest temperature: ${allTemps.reduce(max)}");
   print("lowest temperature: ${allTemps.reduce(min)}");
+  print("The engine ran well ${timesTheEnginRanWell(allTemps)} times");
 }
