b61ff747716e584a6726e2f65591f6e1a17b5dc8
IceMelon1 <avitalnota@gmail.com>
2026-09-10 19:37:25 +0300

    third task complete

--- main.dart
+++ main.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'dart:math';
 
 void main() {
-
   //find the average number in a list
   num avrg(List numbers) {
     num totalTemp = 0;
@@ -18,20 +17,24 @@ void main() {
   //checks how much times the engine temperature was over 25
   int timesTheEnginRanWell(List numbers) {
     int numOfGoodTimes = 0;
-    for(double number in numbers) {
-      if(number > 25) {
+    for (double number in numbers) {
+      if (number > 25) {
         numOfGoodTimes++;
       }
     }
     return numOfGoodTimes;
   }
 
-  //decoding the JSON file
+  //decoding the JSON files
   File file = File('readings.json');
   String stringJson = file.readAsStringSync();
   List<dynamic> listJson = jsonDecode(stringJson);
+  File secondFile = File('Jokes.json');
+  String jokeJson = secondFile.readAsStringSync();
+  Map<String, dynamic> jokesMap = jsonDecode(jokeJson);
 
   List<num> allTemps = [];
+  List<num> lengthOfJokes = [];
 
   //adding all the temperature to a list
   for (Map map in listJson) {
@@ -41,9 +44,22 @@ void main() {
     }
   }
 
+  List<dynamic> jokesList = jokesMap["jokes"];
+  for (dynamic object in jokesList) {
+    Map map = object;
+    if (map["type"] == "single") {
+      lengthOfJokes.add(map["joke"].length);
+    } else {
+      lengthOfJokes.add(map["setup"].length + map["delivery"].length);
+    }
+  }
+
   //prints the answers
   print("Average temperature: ${avrg(allTemps)}");
   print("Highest temperature: ${allTemps.reduce(max)}");
   print("lowest temperature: ${allTemps.reduce(min)}");
   print("The engine ran well ${timesTheEnginRanWell(allTemps)} times");
+  print("Average length of a joke: ${avrg(lengthOfJokes)}");
+  print("The highest length of a joke: ${lengthOfJokes.reduce(max)}");
+  print("The lowest lenght of a joke: ${lengthOfJokes.reduce(min)}");
 }
