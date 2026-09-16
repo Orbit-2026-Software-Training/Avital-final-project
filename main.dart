74a510b8f3dbc654690dbcc1866d40c33b00d96f
Avital Nota <avitalnota@gmail.com>
2026-09-16 21:02:50 +0300

    Revert "sixth task complete"
    This reverts commit 9eb35638b94a9c9612f34085fe57c5d8a9a340bc.

--- main.dart
+++ main.dart
@@ -41,9 +41,6 @@ void main() {
 
   }
 
-  File logFile = File('log.cvs');
-  logFile.writeAsStringSync("Timestamp, Log level, Module, Message\n");
-
   //decodes the JSON files
   File file = File('readings.json');
   String stringJson = file.readAsStringSync();
@@ -51,8 +48,6 @@ void main() {
   File secondFile = File('Jokes.json');
   String jokeJson = secondFile.readAsStringSync();
   Map<String, dynamic> jokesMap = jsonDecode(jokeJson);
-  logFile.writeAsStringSync("${DateTime.now()}, DEBUG, Files, Decoded th JSON files\n", mode: FileMode.append);
-  
 
   List<num> allTemps = [];
   List<num> lengthOfJokes = [];
@@ -66,7 +61,6 @@ void main() {
       allTemps.add(temp.toDouble());
     }
   }
-  logFile.writeAsStringSync("${DateTime.now()}, DEBUG, Engine temperatures, Added all the temperatures to a list\n", mode: FileMode.append);
 
   //splits the jokes from the main JSON and add their length to a list
   List<dynamic> jokesList = jokesMap["jokes"];
@@ -78,7 +72,6 @@ void main() {
       lengthOfJokes.add(map["setup"].length + map["delivery"].length);
     }
   }
-  logFile.writeAsStringSync("${DateTime.now()}, DEBUG, Jokes, Added all the lengths of the jokes to a list\n", mode: FileMode.append);
 
   //adds the error rate to a list, findes from what list the error rate is and if it using the highest or the lowest value of the lists
   errorRates.add(errorRate(allTemps, "min")); 
@@ -97,32 +90,22 @@ void main() {
   } else {
     highestOrLowest = "highest";
   }
-  logFile.writeAsStringSync("${DateTime.now()}, DEBUG, Error rate, Found all the error rates of the engine temperatures and the jokes lengths\n", mode: FileMode.append);
 
   //prints the answers
   print("Average temperature: ${average(allTemps)}");
-  logFile.writeAsStringSync("${DateTime.now()}, DEBUG, Engine temperatures, Found the average engine temperature\n", mode: FileMode.append);
   print("Highest temperature: ${allTemps.reduce(max)}");
-  logFile.writeAsStringSync("${DateTime.now()}, DEBUG, Engine temperatures, Found the highest engine temperature\n", mode: FileMode.append);
   print("Lowest temperature: ${allTemps.reduce(min)}");
-  logFile.writeAsStringSync("${DateTime.now()}, DEBUG, Engine temperatures, Found the lowest engine temperature\n", mode: FileMode.append);
   print("The engine ran well ${timesTheEngineRanWell(allTemps)} times");
-  logFile.writeAsStringSync("${DateTime.now()}, DEBUG, Engine temperatures, Found how much times the engine ran well\n", mode: FileMode.append);
   print("Average length of a joke: ${average(lengthOfJokes)}");
-  logFile.writeAsStringSync("${DateTime.now()}, DEBUG, Jokes, Found the average joke length\n", mode: FileMode.append);
   print("The highest length of a joke: ${lengthOfJokes.reduce(max)}");
-  logFile.writeAsStringSync("${DateTime.now()}, DEBUG, Jokes, Found the highest joke length\n", mode: FileMode.append);
   print("The lowest lenght of a joke: ${lengthOfJokes.reduce(min)}");
-  logFile.writeAsStringSync("${DateTime.now()}, DEBUG, Jokes, Found the lowest joke length\n", mode: FileMode.append);
   print("The highest error rate is ${errorRates.reduce(max)} and it's the $highestOrLowest value of the $tempsOrJokes list");
-  logFile.writeAsStringSync("${DateTime.now()}, DEBUG, Error rate, Found the highest error rate\n", mode: FileMode.append);
 
   //sorts all the temperatures from lowest to highest
   while(allTemps.isNotEmpty) {
     sortedAllTemps.add(allTemps.reduce(min));
     allTemps.remove(allTemps.reduce(min));
   }
-  logFile.writeAsStringSync("${DateTime.now()}, DEBUG, Engine temperatures, Sorted all the engine temperatures form lowest to highest\n", mode: FileMode.append);
 
   //prints the sorted temperatures
   print(sortedAllTemps);
